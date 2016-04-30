df20323d7baf62bcb1d29fe87cb0c5889e5a5338
Jonathan Balls <j.balls@btopenworld.com>
2016-04-30 16:10:36 +0100

    Removed broken socket.io code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // quick setup server.
 var gameport = process.env.PORT || 4004,
-    io      = require('socket.io'),
     express = require('express'),
     UUID    = require('node-uuid'),
     verbose = false,
@@ -23,30 +22,5 @@ app.get('/static/*', function(req, res, next) {
     if(verbose) console.log('\t :: Express :: file requested : ' + file);
 
     res.sendfile(__dirname + '/static/' + file);
-});
-
-
-// Setup socket.io
-var sio = io.listen(app);
-
-sio.configure(function() {
-    sio.set('log level', 0);
-
-    sio.set('authorization', function() {
-        callback(null, true);
-    });
-});
-
-
-// Handle a socket connection
-sio.sockets.on('connection', function(client) {
-    client.userid = UUID();
-    client.emit('onconnected', { id: client.userid });
-    console.log('\t socket.io:: player ' + client.userid + ' disconnected');
-
-    client.on('disconnect', function() {
-        console.log('\t socket.io:: player ' + client.userid + ' disconnected');
-    });
-
 });
 
