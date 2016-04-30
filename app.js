7cf48dc9199df529cb2b087cecbee9eab610899e
Jonathan Balls <j.balls@btopenworld.com>
2016-04-30 14:15:11 +0100

    Barebones file serving

---
+++ app.js
@@ -0,0 +1,28 @@
+// quick setup server.
+
+var gameport = process.env.PORT || 4004,
+    io      = require('socket.io'),
+    express = require('express'),
+    UUID    = require('node-uuid'),
+    verbose = false,
+    app = express();
+
+
+// Setup express
+app.listen(gameport);
+console.log('Listening on port ' + gameport);
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+// Serve static files
+app.get('/*', function(req, res, next) {
+    var file = req.params[0];
+
+    if(verbose) console.log('\t :: Express :: file requested : ' + file);
+
+    res.sendfile(__dirname + '/' + file);
+});
+
+
