8df64fb5f7688f3e3bfb59d758311957e2c985e6
Jonathan Balls <j.balls@btopenworld.com>
2016-04-30 14:44:38 +0100

    Handle socket.io connections

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // quick setup server.
-
 var gameport = process.env.PORT || 4004,
     io      = require('socket.io'),
     express = require('express'),
@@ -16,6 +15,7 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
+
 // Serve static files
 app.get('/static/*', function(req, res, next) {
     var file = req.params[0];
@@ -25,4 +25,27 @@ app.get('/static/*', function(req, res, next) {
     res.sendfile(__dirname + '/static/' + file);
 });
 
+
+// Setup socket.io
+var sio = io.listen(app);
+
+sio.configure(function() {
+    sio.set('log level', 0);
+
+    sio.set('authorization', function() {
+        callback(null, true);
+    });
+});
+
+
+// Handle a socket connection
+sio.sockets.on('connection', function(client) {
+    client.userid = UUID();
+    client.emit('onconnected', { id: client.userid });
+    console.log('\t socket.io:: player ' + client.userid + ' disconnected');
+
+    client.on('disconnect', function() {
+        console.log('\t socket.io:: player ' + client.userid + ' disconnected');
+
+});
 
